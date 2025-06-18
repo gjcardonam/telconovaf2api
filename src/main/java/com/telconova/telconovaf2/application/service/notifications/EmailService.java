b978afe7dd8bf1e637f36621ec10ff845d064eec
Gabriel Cardona <gjcardonam@gmail.com>
2025-06-17 20:56:06 -0500

    email configuration added

--- src/main/java/com/telconova/telconovaf2/application/service/notifications/EmailService.java
+++ src/main/java/com/telconova/telconovaf2/application/service/notifications/EmailService.java
@@ -1,6 +1,6 @@
 package com.telconova.telconovaf2.application.service.notifications;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,13 +10,16 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Autowired
+    @Value("${spring.mail.username}")
+    private String from;
+
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
